cb7431263c6c0da3392c83faddf55869c124708f
Anton Sheko <antonsheko96@gmail.com>
2024-04-17 20:45:42 +0300

    HomeWork_T6/4

---
+++ HomeWork_T6/#4.py
@@ -0,0 +1,6 @@
+# Поиск наибольшего общего делителя
+
+a = int(input('Enter number 1'))
+b = int(input('Enter number 2'))
+import math
+print(f"НОД для чисел {a} и {b} является число", math.gcd(a, b))
