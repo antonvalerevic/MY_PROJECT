97c1198f3bc912ed3f288d10377048f46d7127e6
Anton Sheko <antonsheko96@gmail.com>
2024-04-18 21:42:21 +0300

    HomeWork_T6/2

--- HomeWork_T6/#2.py
+++ HomeWork_T6/#2.py
@@ -1,4 +1,18 @@
 # перевод из десятичной системы в двоичную
 
-n = int(input('Enter number'))
-print(f"The bynary equivalent of {n} is", bin(n))
+
+# n = int(input('Enter number'))
+# print(f"The binary equivalent of {n} is", bin(n))
+
+
+def convert_dec_in_bin(n):
+    bin_num = bin(n)
+    return bin_num
+
+dec_num = 10
+bin_num = convert_dec_in_bin(dec_num)
+bin_n = bin_num.replace('0b', '')
+
+print(f'The binary equivalent of {dec_num} is", {bin_n}')
+
+
