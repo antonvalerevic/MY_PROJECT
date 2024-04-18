f43359ccf5e3cc82da28def16e8d9158a7bc962b
Anton Sheko <antonsheko96@gmail.com>
2024-04-18 21:58:09 +0300

    HomeWork_T6/3

--- HomeWork_T6/#3.py
+++ HomeWork_T6/#3.py
@@ -1,10 +1,26 @@
 # проверка на простое число
 
-n = int(input("Enter number"))
-if n == 2 or n == 3:
-    print(f"Число {n} простое")
-elif n % 2 != 0 and n % 3 != 0:
-    print(f"Число {n} простое")
+# n = int(input("Enter number"))
+# if n == 2 or n == 3:
+#     print(f"Число {n} простое")
+# elif n % 2 != 0 and n % 3 != 0:
+#     print(f"Число {n} простое")
+#
+# if n % 3 == 0 or n % 2 == 0:
+#     print(f"Число {n} не является простым")
 
-if n % 3 == 0 or n % 2 == 0:
+
+def count(n):
+    if n == 0:
+        return False
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+    return True
+
+n = 29
+if count(n):
+    print(f"Число {n} простое")
+else:
     print(f"Число {n} не является простым")
