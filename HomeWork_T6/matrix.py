223acd896f65fa3ade81948911d09be660b25d3c
Anton Sheko <antonsheko96@gmail.com>
2024-05-08 16:48:42 +0300

    HomeWork_T6/matrix

--- HomeWork_T6/matrix.py
+++ HomeWork_T6/matrix.py
@@ -1,19 +1,53 @@
 import numpy as np
-
+# создаем рандомную матрицу 5*5
 def random_matrix(rows, cols):
-    return np.random.rand(rows, cols)
-
-def min_matrix():
-    print(min(matrix))
+    return np.round(np.random.rand(rows, cols), 2)
 
 matrix = random_matrix(5, 5)
-
 print(matrix)
 
+# миниммум и максимум матрицы с индексами
+def min_matrix():
+    return np.min(matrix), np.unravel_index(np.argmin(matrix), matrix.shape)
 
+def max_matrix():
+    return np.max(matrix), np.unravel_index(np.argmax(matrix), matrix.shape)
 
+min_val, min_idx = min_matrix()
+print("min value:", min_val)
+print("min index:", min_idx)
 
+max_val, max_idx = max_matrix()
+print("max value:", max_val)
+print("max index:", max_idx)
 
-min_matrix()
+# сумма матрицы
+def sum_matrix():
+    return sum(sum(matrix))
+sum_m = sum_matrix()
+print("summa", sum_m)
+
+# Определить, какую долю
+# в общей сумме (процент) составляет сумма элементов
+# каждого столбца, столбцы A, B, C, D, E
+
+def A_sum_m():
+    return np.round(sum(([1]), 1) / sum_m * 100, 2)
+def B_sum_m():
+    return np.round(sum(([2]), 1) / sum_m * 100, 2)
+def C_sum_m():
+    return np.round(sum(([3]), 1) / sum_m * 100, 2)
+def D_sum_m():
+    return np.round(sum(([4]), 1) / sum_m * 100, 2)
+def E_sum_m():
+    return np.round(sum(([5]), 1) / sum_m * 100, 2)
+A = A_sum_m()
+B = B_sum_m()
+C = C_sum_m()
+D = D_sum_m()
+E = E_sum_m()
+print("Доля кажого столбца от общей суммы матрицы в %:")
+print(A, B, C, D, E, sep = "%, ")
+
 
 
